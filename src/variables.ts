0e287ec5b42befb041b52e020aa691986ba1255f
Jordan Roher <jordan@notclickable.com>
2023-04-21 16:01:25 -0700

    Which defaults to false

--- src/variables.ts
+++ src/variables.ts
@@ -2,6 +2,6 @@ export const PAGETITLE = "My Website";
 export const PAGEICON = "/logo.png";
 export const SHOWHEADER = true;
 export const SHOWHEADERLINE = true;
-export const SHOWHEADERTOP = true;
+export const SHOWHEADERTOP = false;
 export const CATEGORIES = "normal";
 export const THEME = "light";
