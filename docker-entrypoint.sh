26649e6289bdff0b7028fb9992936a46af3a0624
Jordan Roher <jordan@notclickable.com>
2023-04-21 16:46:52 -0700

    Fixed replacements

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,7 @@ sed -i -e 's/SHOWHEADERTOP = false/SHOWHEADERTOP = '"${HEADERTOP}"'/g' /app/src/
 sed -i -e 's/CATEGORIES = "normal"/CATEGORIES = "'"${CATEGORIES}"'"/g' /app/src/variables.ts
 
 # CSS replacement
-sed -i -e 's/background-color: theme\(colors\.slate\.50\)/background-color: '"${BGCOLOR}"'/g' /app/src/tailwind.css
-sed -i -e 's/background-color: theme\(colors\.gray\.950\)/background-color: '"${BGCOLORDARK}"'/g' /app/src/tailwind.css
+sed -i -e 's/background-color: theme(\(colors\.slate\.50\))/background-color: '"${BGCOLOR}"'/g' /app/src/tailwind.css
+sed -i -e 's/background-color: theme(\(colors\.gray\.950\))/background-color: '"${BGCOLORDARK}"'/g' /app/src/tailwind.css
 
 exec "$@"
