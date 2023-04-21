f7a3edca8c72af0232f785e7b08d53a10a9193d1
Jordan Roher <jordan@notclickable.com>
2023-04-21 15:44:01 -0700

    Default to normal categories

--- src/variables.ts
+++ src/variables.ts
@@ -2,5 +2,5 @@ export const PAGETITLE = "My Website";
 export const PAGEICON = "/logo.png";
 export const SHOWHEADER = true;
 export const SHOWHEADERLINE = true;
-export const CATEGORIES = "small";
+export const CATEGORIES = "normal";
 export const THEME = "light";
