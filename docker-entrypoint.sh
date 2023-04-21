9cb033a39d6cac106bafe2cf4b829d66a6291ee7
Jordan Roher <jordan@notclickable.com>
2023-04-20 21:47:52 -0700

    Typos

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ echo "Replacing HTMLTITLE with ${title} in /app/src/main.tsx"
 sed -i -e 's/HTMLTITLE/'"${TITLE}"'/g' /app/src/main.tsx
 
 # Escape slashes... apparently
-LOGO=${LOGOFILE//\//\\/}
+LOGO=${LOGO//\//\\/}
 
 echo "Replacing LOGO with ${LOGO} in /app/src/main.tsx"
 sed -i -e 's/LOGO/'"${LOGO}"'/g' /app/src/main.tsx
