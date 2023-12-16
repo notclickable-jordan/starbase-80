4b5c3f772bf89f79ffb454619eb992284ce3a3db
Jordan Roher <jordan@notclickable.com>
2023-12-16 13:00:29 -0800

    Removed Vite and most packages Got basic static script working

---
+++ src/static.ts
@@ -0,0 +1,43 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+
+const indexFilePath = path.join(__dirname, "../", "index.html");
+
+async function writeIndexPage(contents: string): Promise<boolean> {
+	return new Promise(async (resolve, reject) => {
+		try {
+			await fs.writeFile(indexFilePath, contents);
+			return resolve(true);
+		} catch (exception) {
+			console.error("Could not write index.html file");
+			reject(exception);
+		}
+	});
+}
+
+async function readIndexPage(): Promise<string> {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const index = await fs.readFile(indexFilePath);
+
+			return resolve(index.toString());
+		} catch (exception) {
+			console.error("Could not read index.html file");
+			reject(exception);
+		}
+	});
+}
+
+async function start(): Promise<void> {
+	const index = await readIndexPage();
+
+	const newText = "<p>Hello, world!</p>";
+	const rootDiv = '<div id="root"></div>';
+	const rootDivReplacement = '<div id="root">$1</div>';
+
+	const newIndex = index.replace(rootDiv, rootDivReplacement.replace("$1", newText));
+
+	await writeIndexPage(newIndex);
+}
+
+start();
