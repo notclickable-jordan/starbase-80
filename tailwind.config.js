2908b36ecd74f87834da5fc27771177c117d0fc6
Jordan Roher <jordan@notclickable.com>
2023-12-17 22:20:44 -0800

    Configured Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./dist/index.html"],
 	safelist: [
 		/* Built from icon.tsx */
 		{
